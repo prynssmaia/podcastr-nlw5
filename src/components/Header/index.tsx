d288b1e89963f6fd4ff90a9303b5455ad2a823a9
Prynss <prynss_lp@hotmail.com>
2021-04-20 22:36:42 -0300

    add: player header and emptyPlayer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,6 @@
 import styles from './styles.module.scss';
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
-import { PRIORITY_BELOW_NORMAL } from 'node:constants';
-
 
 export function Header() {
     const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
@@ -11,7 +9,7 @@ export function Header() {
 
 
     return(
-        <header className={styles.headerConteiner}>
+        <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcastr"/>
             <p>O melhor para você ouvir, sempre.</p>
             <span>{currentDate}</span>            
