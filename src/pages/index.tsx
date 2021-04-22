7d64c1ba4443ce08851cec351b5d0a041a1800e4
Prynss <prynss_lp@hotmail.com>
2021-04-22 20:45:14 -0300

    add: type episode configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,12 @@ type Episode = {
     id: string;
     title: string;
     members: string;
-    published_at: string;
     thumbnail: string;
     description: string;
-    file: {
-      url: string;
-      type: string;
-      duration: number
-    }
+    duration: number;
+    durationAsString: string;
+    url: string;
+    publishedAt: string;
 }
 
 type HomeProps = {
