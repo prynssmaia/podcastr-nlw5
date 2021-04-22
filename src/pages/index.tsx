a009aa07be1359fbd2fb97dfb5c8e226181bd6a2
prynssmaia <prynss_lp@hotmail.com>
2021-04-22 11:39:10 -0300

    add: axios api cofiguration and params

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
 type Episode = {
     id: string;
@@ -30,8 +31,14 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+  const response = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'public',
+      _order: 'desc'
+    }
+  })
+  const data = response.data
 
   return{
     props: {
