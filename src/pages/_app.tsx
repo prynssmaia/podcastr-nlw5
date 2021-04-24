ab0a785017c8b69fb12fae3b6f14ce2abd0b36fe
Prynss <prynss_lp@hotmail.com>
2021-04-24 20:39:22 -0300

    add: create Player context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,29 @@ import { Player } from '../components/Player';
 import { Header } from '../components/Header';
 import styles from '../styles/app.module.scss';
 import '../styles/global.scss';
+import { PlayerContext } from '../contexts/PlayerContext';
+import { useState } from 'react';
 
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpidosdeList] = useState([])
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  
+  function play(episode) {
+    setEpidosdeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
   return (
-    <div className={styles.appWrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.appWrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>    
   ) 
 }
 
