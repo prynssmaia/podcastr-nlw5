851ed52e09de36c27e1cd2a53b4b4fad3841339e
Prynss <prynss_lp@hotmail.com>
2021-04-23 23:52:03 -0300

    add: config and styled episode page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,9 +1,12 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import styles from './episode.module.scss';
 
 type Episode = {
     id: string;
@@ -22,11 +25,38 @@ type EpisodeProps = {
 } 
 
 export default function Episode({ episode }: EpisodeProps){
-    const router = useRouter();
 
-    return(
-        <h1>{episode.title}</h1>
-    )
+    return (
+      <div className={styles.episode}>
+        <div className={styles.thumbnailContainer}>
+          <Link href="/">
+              <button type="button">
+                <img src="/arrow-left.svg" alt="Voltar" />
+              </button>
+          </Link>
+          <Image
+            className={styles.thumbnailImage}
+            width={700}
+            height={160}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <button type="button">
+              <img src="/play.svg" alt="Tocar  episódio"/>
+          </button>
+        </div>
+
+        <header>
+            <h1>{episode.title}</h1>
+            <span>{episode.members}</span>
+            <span>{episode.publishedAt}</span>
+            <span>{episode.durationAsString}</span>
+        </header>
+
+        <div className={styles.description} dangerouslySetInnerHTML={{__html: episode.description }}/>
+
+      </div>
+    );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
