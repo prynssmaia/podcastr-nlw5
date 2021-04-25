4f88b226681c61e47138928630bcf19edfb1968f
Prynss <prynss_lp@hotmail.com>
2021-04-24 21:09:54 -0300

    add: styled player component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,7 +2,8 @@ import { useContext } from 'react';
 import Image from 'next/image';
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
-
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 export function Player() {
     const {episodeList,currentEpisodeIndex} = useContext(PlayerContext)
@@ -18,7 +19,7 @@ export function Player() {
 
         {episode ? (
           <div className={styles.currentEpisode}>
-            <Image
+            <Image className={styles.thumbnailPlayer}
               width={592}
               height={592}
               src={episode.thumbnail}
@@ -33,11 +34,20 @@ export function Player() {
           </div>
         )}
 
-        <footer className={styles.empty}>
+        <footer className={!episode ? styles.empty : ''}>
           <div className={styles.progress}>
             <span>00:00</span>
             <div className={styles.slider}>
-              <div className={styles.emptySlider} />
+              { episode? (
+                  <Slider 
+                    trackStyle={{ backgroundColor: '#04d361'}}
+                    railStyle={{ backgroundColor: '#9f75ff'}}
+                    handleStyle={{ borderColor: '#04d361', borderWidth: 4}}
+
+                    />
+              ) : (
+                  <div className={styles.emptySlider} />
+              )}
             </div>
             <span>00:00</span>
           </div>
