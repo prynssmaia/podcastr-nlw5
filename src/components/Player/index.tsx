da12b956ec36a93eeb1645613db8dd6762057b35
Prynss <prynss_lp@hotmail.com>
2021-04-24 21:47:02 -0300

    add: play and pause functionality

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
@@ -6,7 +6,26 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 export function Player() {
-    const {episodeList,currentEpisodeIndex} = useContext(PlayerContext)
+    const audioRef = useRef<HTMLAudioElement>(null)
+
+    const {
+      episodeList,
+      currentEpisodeIndex,
+      isPlaying,
+      togglePlay,
+    } = useContext(PlayerContext);
+
+    useEffect(() => {
+        if(!audioRef.current) {
+            return;
+        }
+
+        if(isPlaying) {
+            audioRef.current.play()
+        } else {
+            audioRef.current.pause()
+        }
+    }, [isPlaying]) 
 
     const episode = episodeList[currentEpisodeIndex]
 
@@ -19,7 +38,8 @@ export function Player() {
 
         {episode ? (
           <div className={styles.currentEpisode}>
-            <Image className={styles.thumbnailPlayer}
+            <Image
+              className={styles.thumbnailPlayer}
               width={592}
               height={592}
               src={episode.thumbnail}
@@ -34,30 +54,24 @@ export function Player() {
           </div>
         )}
 
-        <footer className={!episode ? styles.empty : ''}>
+        <footer className={!episode ? styles.empty : ""}>
           <div className={styles.progress}>
             <span>00:00</span>
             <div className={styles.slider}>
-              { episode? (
-                  <Slider 
-                    trackStyle={{ backgroundColor: '#04d361'}}
-                    railStyle={{ backgroundColor: '#9f75ff'}}
-                    handleStyle={{ borderColor: '#04d361', borderWidth: 4}}
-
-                    />
+              {episode ? (
+                <Slider
+                  trackStyle={{ backgroundColor: "#04d361" }}
+                  railStyle={{ backgroundColor: "#9f75ff" }}
+                  handleStyle={{ borderColor: "#04d361", borderWidth: 4 }}
+                />
               ) : (
-                  <div className={styles.emptySlider} />
+                <div className={styles.emptySlider} />
               )}
             </div>
             <span>00:00</span>
           </div>
 
-          { episode && (
-              <audio
-                src={episode.url}
-                autoPlay  
-              />
-          )}
+          {episode && <audio src={episode.url} ref={audioRef} autoPlay />}
 
           <div className={styles.buttons}>
             <button type="button" disabled={!episode}>
@@ -66,8 +80,17 @@ export function Player() {
             <button type="button" disabled={!episode}>
               <img src="/play-previous.svg" alt="Tocar anterior" />
             </button>
-            <button type="button" className={styles.playButton} disabled={!episode}>
-              <img src="/play.svg" alt="Tocar" />
+            <button
+              type="button"
+              className={styles.playButton}
+              disabled={!episode}
+              onClick={togglePlay}
+            >
+              {isPlaying ? (
+                <img src="/pause.svg" alt="Pause" />
+              ) : (
+                <img src="/play.svg" alt="Tocar" />
+              )}
             </button>
             <button type="button" disabled={!episode}>
               <img src="/play-next.svg" alt="Tocar próxima" />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,20 @@ import { useState } from 'react';
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpidosdeList] = useState([])
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
   
   function play(episode) {
     setEpidosdeList([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying)
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay }}>
       <div className={styles.appWrapper}>
         <main>
           <Header />
